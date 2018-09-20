47d2aba45bb2edd1b39761050178758fc19cc677
Ankita Pasad <ankitapasad24@gmail.com>
2018-09-19 20:19:30 -0500

    minor changes

--- fromTranscripts_bow.py
+++ fromTranscripts_bow.py
@@ -48,17 +48,17 @@ if sys.argv[1]=='dataproc':
             index = [] # bag of words feature for this utterance
             words = line.split('\t')[1].split(' ') # list of words
             tag = line.split('\t')[2]
-            if(tag=='statement' or tag=='backchannel'):
-                if(dataSplit=='train' and tag not in tag2label):
-                    tag2label[tag] = labelId
-                    labelId += 1
-                    # labelWeights.append(tag2freq[tag])
-                labels.append(tag2label[tag])
-                for word in words:
-                    try: index.append(vocab.index(word))
-                    except: index.append(0) # for 'UNK'
-                indices.append(index)
-                dataSize += 1
+            # if(tag=='statement' or tag=='backchannel'):
+            if(dataSplit=='train' and tag not in tag2label):
+                tag2label[tag] = labelId
+                labelId += 1
+                # labelWeights.append(tag2freq[tag])
+            labels.append(tag2label[tag])
+            for word in words:
+                try: index.append(vocab.index(word))
+                except: index.append(0) # for 'UNK'
+            indices.append(index)
+            dataSize += 1
 
         data = np.zeros([dataSize,len(vocab)])
         dataTensor = [None]*dataSize
@@ -91,16 +91,17 @@ if sys.argv[1]=='dataproc':
     trainDataTensor, trainLabels, vocab, tag2label = getData('train',tag2freq=tag2freq)
     devDataTensor, devLabels = getData('dev',vocab,tag2label)
     testDataTensor, testLabels = getData('test',vocab,tag2label) 
+    print(tag2label)
 
-    print('Saving data')
-    torch.save(trainDataTensor, 'trainData.pt')
-    torch.save(trainLabels, 'trainLabels.pt')
+    # print('Saving data')
+    # torch.save(trainDataTensor, 'trainData.pt')
+    # torch.save(trainLabels, 'trainLabels.pt')
 
-    torch.save(devDataTensor, 'devData.pt')
-    torch.save(devLabels, 'devLabels.pt')
+    # torch.save(devDataTensor, 'devData.pt')
+    # torch.save(devLabels, 'devLabels.pt')
 
-    torch.save(testDataTensor, 'testData.pt')
-    torch.save(testLabels, 'testLabels.pt')
+    # torch.save(testDataTensor, 'testData.pt')
+    # torch.save(testLabels, 'testLabels.pt')
 
 
 elif sys.argv[1]=='train':
@@ -109,8 +110,8 @@ elif sys.argv[1]=='train':
     trainData = torch.stack(trainData)
     trainLabels = torch.load('trainLabels.pt')
     # debugging experimentation: overfits?
-    trainData = trainData[:100]
-    trainLabels = trainLabels[:100]
+    # trainData = trainData[:100]
+    # trainLabels = trainLabels[:100]
     devData = torch.stack(torch.load('devData.pt'))
     devLabels = torch.load('devLabels.pt')
     testData = torch.stack(torch.load('testData.pt'))
@@ -118,7 +119,7 @@ elif sys.argv[1]=='train':
 
     inSize = len(open('vocab.txt','rb').readlines())
 
-    nClasses = 2
+    nClasses = 43
     nHidden = 256
     # weights = torch.zeros(nClasses)
     # weightFile = open('labelWeights.txt','r').readlines()
@@ -126,14 +127,16 @@ elif sys.argv[1]=='train':
     #     weights[i] = (1-float(weightFile[i].strip('\n')))/(nClasses-1)
 
     class Net(nn.Module):
-        def __init__(self,nClasses,inputSize,hiddenSize,init=False):
+        def __init__(self,nClasses,inputSize,hiddenSize,nLayers=1,init=False):
             super(Net, self).__init__()
 
             # self.embed = nn.Embedding(inputSize,hiddenSize1)
-            self.affine1 = nn.Linear(inputSize,hiddenSize)
-            self.affine2 = nn.Linear(hiddenSize,int(hiddenSize/4))
-            self.affine3 = nn.Linear(int(hiddenSize/4),nClasses)
-
+            self.nLayers = nLayers
+            self.affineIn = nn.Linear(inputSize,hiddenSize)
+            self.affineOut = nn.Linear(hiddenSize,nClasses)
+            self.hiddenLayers = nn.Sequential()
+            for i in range(nLayers):
+                self.hiddenLayers.add_module('layer'+str(i), nn.Linear(hiddenSize, hiddenSize))
             if(init):
                 for m in self.modules():
                     if isinstance(m, nn.Linear):
@@ -141,71 +144,87 @@ elif sys.argv[1]=='train':
                         nn.init.constant_(m.bias, 0)
 
         def forward(self,x):
-            out = F.relu(self.affine1(x))
-            out = F.relu(self.affine2(out))
-            out = F.relu(self.affine3(out))
+            out = F.sigmoid(self.affineIn(x))
+            out = F.sigmoid(self.hiddenLayers.forward(out))
+            out = F.sigmoid(self.affineOut(out))
 
             return out
 
-    ff = Net(nClasses,inSize,nHidden,init=False).cuda()
-    # optimizer = optim.Adam(ff.parameters(),lr=0.005)
+    ff = Net(nClasses,inSize,nHidden,nLayers=1,init=False).cuda()
+    # optimizer = optim.Adam(ff.parameters(),lr=0.01)
     # optimizer = optim.SGD(ff.parameters(),lr=0.1)
     optimizer = optim.Adagrad(ff.parameters(),lr=0.01)
     criterion = torch.nn.CrossEntropyLoss(size_average=False).cuda()
 
     nEpoch = 10
     iterNo = 0
-    b = 2
+    batchSize = 30
     trainLoss = []
     devLoss = []
     trainAcc = []
     devAcc = []
+    weightNormDiff = []
+
+    def validationStep(ff,devAcc,devLoss):
+
+        totalDev = len(devData)
+        correctDev = 0
+        lossTotDev = 0
+        for i in range(totalDev):
+            input = devData[i].cuda()
+            output = ff(input)
+            lossTotDev += criterion(output.view(1,nClasses), torch.tensor([devLabels[i]]).cuda()).detach()
+            pred = np.argmax(output.cpu().detach().numpy())
+            if(pred==devLabels[i]): correctDev += 1
+        print("Val acc: ", correctDev/float(totalDev))
+        devAcc.append(correctDev/float(totalDev))
+        devLoss.append(lossTotDev/float(totalDev))
+
+        return devAcc, devLoss
+
+
     ff.train()
+    print(ff.affineIn.weight)
     while(iterNo<nEpoch):
         print("Epoch no: ", iterNo)
-        total = len(trainData)
+        # print(ff.training)
+        # total = len(trainData)
+        total = 0
         correct = 0
         lossTot = 0
-        nBatches = len(trainData)//b
+        nBatches = len(trainData)//batchSize
         startIdx = 0
         for batchIdx in range(nBatches):
-            start, end = batchIdx*b, (batchIdx+1)*b
+            optimizer.zero_grad()
+            total += batchSize
+            start, end = batchIdx*batchSize, (batchIdx+1)*batchSize
             data, target = trainData[start:end].cuda(), torch.tensor(trainLabels[start:end]).cuda()
             # input = trainData[i]
             output = ff(data)
-            output = output.view(b,nClasses)
+            output = output.view(batchSize,nClasses)
             # loss = criterion(output.view(1,37), torch.tensor([trainLabels[i]]))
             loss = criterion(output, target)
             loss.backward()
             optimizer.step()
-            optimizer.zero_grad()
+            if(batchIdx==0): prevStepWeight = ff.affineIn.weight.detach().cpu().numpy()
             lossTot += loss.detach()
             pred = torch.max(output, 1)[1]
             correct += torch.sum(pred==target).cpu().numpy()
         #     pred = np.argmax(output.detach().cpu().numpy())
         #     if(pred==trainLabels[i]): correct += 1
+            if(((batchIdx+1)%(nBatches//4))==0):
+                # print(np.count_nonzero(list(ff.parameters())[0].grad.detach().cpu().numpy()))
+                print("Train acc: ", correct/float(total))
+                # print(ff.affineIn.weights.grad)
+                # print(pred)
+                weightNormDiff.append(np.sqrt(np.sum((ff.affineIn.weight.detach().cpu().numpy() - prevStepWeight)**2)))
+                prevStepWeight = ff.affineIn.weight.detach().cpu().numpy()
+                trainAcc.append(correct/float(total))
+                trainLoss.append(lossTot/float(total))
 
-        print("Train acc: ", correct/float(total))
-        trainAcc.append(correct/float(total))
-        trainLoss.append(lossTot/float(total))
+                devAcc, devLoss = validationStep(ff,devAcc,devLoss)
 
-
-        ff.eval()
-        total = len(devData)
-        correct = 0
-        lossTot = 0
-        for i in range(total):
-            input = devData[i].cuda()
-            output = ff(input)
-            lossTot += criterion(output.view(1,nClasses), torch.tensor([devLabels[i]]).cuda()).detach()
-            pred = np.argmax(output.cpu().detach().numpy())
-            if(pred==devLabels[i]): correct += 1
-        print("Val acc: ", correct/float(total))
-        devAcc.append(correct/float(total))
-        devLoss.append(lossTot/float(total))
         iterNo += 1
-
-        ff.train()
 
     ff.eval()
     total = len(testData)
@@ -221,7 +240,7 @@ elif sys.argv[1]=='train':
     fig, ax1 = plt.subplots()
     ax1.plot(xaxis,trainLoss,'g',label='train loss')
     ax1.plot(xaxis,devLoss,'r',label='dev loss')
-    ax1.set_xlabel('Number of epochs')
+    ax1.set_xlabel('Number of quarter epochs')
     ax1.set_ylabel('Negative log-likelihood', color='g')
     plt.legend(loc=6)
 
@@ -232,7 +251,13 @@ elif sys.argv[1]=='train':
     plt.legend(loc=7)
 
     fig.tight_layout()
-    plt.savefig('LossCompare_dm.png')
+    plt.savefig('LossCompare.png')
+
+    plt.figure()
+    plt.plot(weightNormDiff)
+    plt.xlabel("number of quarter epochs")
+    plt.ylabel("norm difference in learned weights")
+    plt.savefig('weightNorm.png')
     # plt.show()
     # plt.close()
 
